968d34d5bdd065500b33dd83c83541560e5d00eb
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-24 10:02:04 -0700

    Define state for sign-in in app.js

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ var studentSignIn = angular.module('studentSignIn', ['ui.router']);
       url: '',
       templateUrl: 'partials/home.html'
     });
+
+    $stateProvider.state('sign_in', {
+      url: '/sign_in',
+      templateUrl: 'partials/sign_in.html',
+      controller: 'StatusCtrl'
+    });
   });
