078af1bdddb98674023f50b61b190b73b83b02eb
Kyle Giard-Chase <kgiardch@gmail.com>
2015-04-28 13:22:35 -0700

    Add functionality to add or delete a student.

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,3 +1,15 @@
 studentSignIn.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory) {
     $scope.students = StudentsFactory.students;
+
+    $scope.addStudent = function() {
+      var name = $scope.studentName;
+      StudentsFactory.addStudent(name);
+      $scope.studentName = null;
+    };
+
+    $scope.deleteStudent = function() {
+      var name = $scope.studentName;
+      StudentsFactory.deleteStudent(name);
+    };
+    
 });
--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,12 +1,15 @@
 studentSignIn.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
-  factory.students = [
-    { name: "Kyle", present: false },
-    { name: "Maggie", present: false },
-    { name: "Erik", present: false },
-    { name: "Bryant", present: false },
-    { name: "Gillian", present: false }
-  ]
+  factory.students = [];
+
+  factory.addStudent = function(newName) {
+   this.students.push({name: newName, present: false });
+  };
+
+  factory.deleteStudent = function(student) {
+   var index = this.students.indexOf(student);
+   this.students.splice(index, 1);
+  };
 
   factory.signIn = function(student) {
     student.present = true;
